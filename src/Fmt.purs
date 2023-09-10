7d1fa7e15c6855bc421ec9b1c42054b15c1214d1
Michael Bock <no-day@posteo.net>
2023-09-10 07:03:10 +0200

    Add config

--- src/Fmt.purs
+++ src/Fmt.purs
@@ -16,20 +16,39 @@ import Prim.Symbol as Sym
 import Record as Record
 import Type.Data.Boolean (class If)
 import Type.Data.List (type (:>), List', Nil')
+import Type.Equality (class TypeEquals)
 import Type.Proxy (Proxy(..))
 
 --------------------------------------------------------------------------------
 
-t1 :: String
-t1 = fmt @"Hello, my name is {name}. I live in {city}."
-  { name: "Tom", city: "London" }
+greeting :: String
+greeting =
+  fmt
+    @"""
+      Hello, my name is {name}. I live in {city}.   
+      Hello, my name is {name}. I live in {city}.    
+      Hello, my name is {name}. I live in {city}.    
+      Hello, my name is {name}. I live in {city}.    
+      Hello, my name is {name}. I live in {city}.    
+      Hello, my name is {name}. I live in {city}.    
+      Hello, my name is {name}. I live in {city}.    
+      Hello, my name is {name}. I live in {city}.    
+      Hello, my name is {name}. I live in {city}.    
+      Hello, my name is {name}. I live in {city}.    
+
+    """
+    { name: "Tom", city: "London" }
 
 --------------------------------------------------------------------------------
 --- fmt
 --------------------------------------------------------------------------------
 
-fmt :: forall @sym replace. Format sym replace => replace -> String
-fmt = format (Proxy :: _ sym)
+fmt
+  :: forall @sym replace
+   . Format DefaultConfig sym replace
+  => replace
+  -> String
+fmt = format (Proxy :: _ DefaultConfig) (Proxy :: _ sym)
 
 --------------------------------------------------------------------------------
 --- SymToList
@@ -46,38 +65,75 @@ else instance
   SymToList sym (head :> xs)
 
 --------------------------------------------------------------------------------
+--- Config
+--------------------------------------------------------------------------------
+
+foreign import data ConfigOpen :: Type
+foreign import data MkConfigOpen :: Symbol -> ConfigOpen
+
+foreign import data MkConfigClose :: Symbol -> ConfigClose
+foreign import data ConfigClose :: Type
+
+foreign import data ConfigToString :: Type
+foreign import data MkConfigToString :: Type -> ConfigToString
+
+foreign import data Config :: Type
+foreign import data MkConfig :: ConfigOpen -> ConfigClose -> ConfigToString -> Config
+
+type DefaultConfig = MkConfig
+  (MkConfigOpen "{")
+  (MkConfigClose "}")
+  (MkConfigToString DefaultToString)
+
+--------------------------------------------------------------------------------
 --- Fmt
 --------------------------------------------------------------------------------
 
-class Format (sym :: Symbol) (replace :: Type) where
-  format :: Proxy sym -> replace -> String
+class Format (config :: Config) (sym :: Symbol) (replace :: Type) where
+  format :: Proxy config -> Proxy sym -> replace -> String
 
 instance
   ( SymToList sym tokens
-  , ParseNamed tokens row
+  , ParseNamed config tokens row
   , IsSymbol sym
-  , ReplaceMap row
+  , ReplaceMap config row
+  , TypeEquals config (MkConfig (MkConfigOpen open) (MkConfigClose close) configToString)
+  , IsSymbol open
+  , IsSymbol close
   ) =>
-  Format sym (Record row)
+  Format config sym (Record row)
   where
-  format _ record =
+  format _ _ record =
     let
       initStr :: String
       initStr = reflectSymbol (Proxy :: Proxy sym)
 
       replacements :: Array (String /\ String)
-      replacements = replaceMap record
-    in
-      foldr replaceNamed initStr replacements
+      replacements = replaceMap (Proxy :: Proxy config) record
 
-replaceNamed :: (String /\ String) -> String -> String
-replaceNamed (key /\ value) = Str.replace
-  (Pattern ("{" <> key <> "}"))
+      replaceConfig =
+        { open: reflectSymbol (Proxy :: Proxy open)
+        , close: reflectSymbol (Proxy :: Proxy close)
+        }
+    in
+      foldr (replaceNamed replaceConfig) initStr replacements
+
+replaceNamed :: { open :: String, close :: String } -> (String /\ String) -> String -> String
+replaceNamed { open, close } (key /\ value) = Str.replaceAll
+  (Pattern (open <> key <> close))
   (Replacement value)
 
 --------------------------------------------------------------------------------
 --- ToString
 --------------------------------------------------------------------------------
+
+class ToStringBy (tok :: Type) (a :: Type) where
+  toStringBy :: Proxy tok -> a -> String
+
+data DefaultToString
+
+instance (ToString a) => ToStringBy DefaultToString a where
+  toStringBy _ = toString
 
 class ToString (a :: Type) where
   toString :: a -> String
@@ -98,35 +154,42 @@ instance ToString Char where
 --- ReplaceMap
 --------------------------------------------------------------------------------
 
-class ReplaceMap row where
-  replaceMap :: Record row -> Array (String /\ String)
+class
+  ReplaceMap
+    (config :: Config)
+    (row :: Row Type)
+  where
+  replaceMap :: Proxy config -> Record row -> Array (String /\ String)
 
 instance
   ( RowToList row rowlist
-  , ReplaceMapRL rowlist row
+  , ReplaceMapRL config rowlist row
   ) =>
-  ReplaceMap row where
-  replaceMap = replaceMapRL (Proxy :: Proxy rowlist)
+  ReplaceMap config row
+  where
+  replaceMap _ = replaceMapRL (Proxy :: Proxy config) (Proxy :: Proxy rowlist)
 
 class
   ReplaceMapRL
+    (config :: Config)
     (rowlist :: RowList Type)
     (row :: Row Type)
   where
-  replaceMapRL :: Proxy rowlist -> Record row -> Array (String /\ String)
+  replaceMapRL :: Proxy config -> Proxy rowlist -> Record row -> Array (String /\ String)
 
-instance ReplaceMapRL RL.Nil row where
-  replaceMapRL _ _ = []
+instance ReplaceMapRL config RL.Nil row where
+  replaceMapRL _ _ _ = []
 
 instance
-  ( ReplaceMapRL tail row
-  , ToString value
+  ( ReplaceMapRL config tail row
+  , ToStringBy configToString value
   , IsSymbol key
   , Row.Cons key value row' row
+  , TypeEquals config (MkConfig configOpen configClose (MkConfigToString configToString))
   ) =>
-  ReplaceMapRL (RL.Cons key value tail) row
+  ReplaceMapRL config (RL.Cons key value tail) row
   where
-  replaceMapRL _ record =
+  replaceMapRL _ _ record =
     let
       key :: String
       key = reflectSymbol (Proxy :: Proxy key)
@@ -135,10 +198,10 @@ instance
       value = Record.get (Proxy :: Proxy key) record
 
       valueStr :: String
-      valueStr = toString value
+      valueStr = toStringBy (Proxy :: Proxy configToString) value
 
       tail :: Array (String /\ String)
-      tail = replaceMapRL (Proxy :: Proxy tail) record
+      tail = replaceMapRL (Proxy :: Proxy config) (Proxy :: Proxy tail) record
     in
       tail `Array.snoc` (key /\ valueStr)
 
@@ -148,15 +211,51 @@ instance
 
 class
   ParseNamed
+    (config :: Config)
     (tokens :: List' Symbol)
     (replace :: Row Type)
   | tokens -> replace
 
-instance ParseNamed Nil' ()
+instance ParseNamed config Nil' ()
 
-instance (ParseId tokens tokens replace "") => ParseNamed ("{" :> tokens) replace
+else instance
+  ( TypeEquals config (MkConfig (MkConfigOpen open) (MkConfigClose close) configToString)
+  , SymToList open openTokens
+  , ParseOpen config openTokens tokens replace
+  ) =>
+  ParseNamed config tokens replace
 
-else instance (ParseNamed tokens replace) => ParseNamed (head :> tokens) replace
+--------------------------------------------------------------------------------
+--- ParseSym
+--------------------------------------------------------------------------------
+
+class ParseOpen (config :: Config) (open :: List' Symbol) (tokens :: List' Symbol) (replace :: Row Type)
+
+instance
+  ( ParseId config tokens tokens replace ""
+  ) =>
+  ParseOpen config Nil' tokens replace
+
+instance
+  ( ParseOpen config tailSym tailTokens replace
+  ) =>
+  ParseOpen config (head :> tailSym) (head :> tailTokens) replace
+
+else instance
+  ( ParseNamed config tailTokens replace
+  ) =>
+  ParseOpen config (head1 :> tailSym) (head2 :> tailTokens) replace
+
+---
+
+testParseSym :: forall sym tokensIn tokensOut. Proxy sym -> Proxy tokensIn -> Proxy tokensOut -> Unit
+testParseSym _ _ _ = unit
+
+t1 :: Unit
+t1 = testParseSym
+  (Proxy :: _ ("a" :> "b" :> Nil'))
+  (Proxy :: _ ("a" :> "b" :> "c" :> Nil'))
+  (Proxy :: _ ("c" :> Nil'))
 
 --------------------------------------------------------------------------------
 --- ParseId
@@ -164,33 +263,35 @@ else instance (ParseNamed tokens replace) => ParseNamed (head :> tokens) replace
 
 class
   ParseId
+    (config :: Config)
     (tokens :: List' Symbol)
     (backtrack :: List' Symbol)
     (replace :: Row Type)
     (id :: Symbol)
 
-instance ParseId Nil' tokens replace id
+instance ParseId config Nil' tokens replace id
 
 instance
-  ( ParseNamed tokens replace
+  ( ParseNamed config tokens replace
   , Row.Cons id typ replace replace'
   , Row.Nub replace' replace''
+  , TypeEquals config (MkConfig (MkConfigOpen open) (MkConfigClose close) configToString)
   ) =>
-  ParseId ("}" :> tokens) backtrack replace'' id
+  ParseId (MkConfig (MkConfigOpen open) (MkConfigClose close) configToString) (close :> tokens) backtrack replace'' id
 
 else instance
   ( IsAlpha head bool
   , If bool tokens Nil' a_xs
 
   , Sym.Append id head id'
-  , ParseId a_xs backtrack a_replace id'
+  , ParseId config a_xs backtrack a_replace id'
 
   , If bool Nil' tokens b_xs
-  , ParseNamed b_xs b_replace
+  , ParseNamed config b_xs b_replace
 
   , If bool a_replace b_replace replace
   ) =>
-  ParseId (head :> tokens) backtrack replace id
+  ParseId config (head :> tokens) backtrack replace id
 
 --------------------------------------------------------------------------------
 --- IsAlpha
@@ -228,4 +329,4 @@ else instance IsAlpha "w" True
 else instance IsAlpha "x" True
 else instance IsAlpha "y" True
 else instance IsAlpha "z" True
-else instance IsAlpha c False
+else instance IsAlpha other False
